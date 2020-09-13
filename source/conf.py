9aecb5c0a02e194f066e1761e722f6e28b8c5ffd
Seth Fischer <seth@fischer.nz>
2020-09-13 12:04:03 +1200

    Format code

--- source/conf.py
+++ source/conf.py
@@ -1,17 +1,16 @@
-project = 'Nissan Leaf OBD-II guide'
-copyright = '2020, Seth Fischer'
-author = 'Seth Fischer'
+project = "Nissan Leaf OBD-II guide"
+copyright = "2020, Seth Fischer"
+author = "Seth Fischer"
 
-release = '0.1.0'
+release = "0.1.0"
 
-extensions = [
-]
+extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 exclude_patterns = []
 
 html_show_copyright = False
 html_show_sphinx = False
-html_static_path = ['_static']
-html_theme = 'sphinx_rtd_theme'
+html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
