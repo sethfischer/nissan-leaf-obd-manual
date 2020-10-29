9d71c8adec6ff64dd84c8d645ab4469c06b88c48
Seth Fischer <seth@fischer.nz>
2020-10-29 20:43:11 +1300

    GitHub workflow: Lint prose

--- lint-prose.sh
+++ lint-prose.sh
@@ -4,4 +4,10 @@ set -o errexit -o noclobber -o nounset
 
 directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-vale --config "${directory}/.vale.ini" "${directory}/source/"
+ci_alert_level=""
+
+if [ -n "${CI+x}" ]; then
+    ci_alert_level="--minAlertLevel=error"
+fi
+
+vale --config "${directory}/.vale.ini" ${ci_alert_level} "${directory}/source/"
