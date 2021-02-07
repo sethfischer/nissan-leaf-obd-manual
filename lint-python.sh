41626ac8103ef510084b93ede73420aa05b0b97e
Seth Fischer <seth@fischer.nz>
2021-02-07 22:16:34 +1300

    Run isort --check-only on Python files

--- lint-python.sh
+++ lint-python.sh
@@ -6,3 +6,4 @@ directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null flake8
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null black --check
+git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null isort --check-only
